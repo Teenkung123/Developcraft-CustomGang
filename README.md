5681a782d2bc0eae8d9055250005e4576ced377c
BuildTools <unconfigured@null.spigotmc.org>
2022-07-04 01:21:36 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This plugin is used for Developcraft Network server
+Please don't compile this plugin except you got permission from me to do it
---
+++ src/main/java/com/teenkung/devgang/DevGang.java
@@ -0,0 +1,17 @@
+package com.teenkung.devgang;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DevGang extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
